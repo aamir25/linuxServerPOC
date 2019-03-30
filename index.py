17d2c5b6d989d9cd938364ce3c011db28dce9fa2
aamir25 <aam.khan1996@gmail.com>
2019-03-29 20:16:57 -0700

    second commit

---
+++ index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Turn on debug mode
+import cgitb
+cgitb.enable()
+
+#Print required headers
+print("Content-Type:application/json;charset=utf-8")
+print()
+print("Hello World")
